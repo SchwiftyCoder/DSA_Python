7d3f9d41b1901bda230ee4f069b05a85b6a40bbc
SchiwftyCoder <adomprince@gmail.com>
2023-12-11 06:36:48 -0500

    Lettcode Problem 387_FirstUniqueCharacter | EASY
    wrote two different approaches to solving this problem. Both run in O(n)
    time but the second approach runs slightly better because only one for
    loop is used instead of two in former approach

--- Hashtables/leetcode/387_FirstUniqueCharacter.py
+++ Hashtables/leetcode/387_FirstUniqueCharacter.py
@@ -52,6 +52,23 @@ class Solution:
         return -1
 
 
+
+    # slightly faster than the method above
+    def firstUniqueChar_v2(self, s: str) -> int:
+        # all 26 lower case letters have an initial 0 occurence
+        charCount = [0] * 26
+
+        # runs in O(1) time despite beig a loop
+        # ord() also runs in O(1) time
+        for char in s:
+            charCount[ord(char) - ord('a')] += 1
+
+        # runs in O(n) time
+        # find the inde where the count of the character is exactly 1
+        for index, char in enumerate(s):
+            if charCount[ord(char) - ord('a')] == 1:
+                return index
+
 sol = Solution()
 print(sol.firstUniqueChar("aabb"))
 print(sol.firstUniqueChar("leetcode"))
